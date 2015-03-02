a2fbf4a274166f1d337951c063812b8c63fc8ef1
Mark A. Gibbs <indi.in.the.wired@gmail.com>
2015-03-02 03:57:28 -0500

    Added EPUB build makefile from https://github.com/DarkerStar/epub-build-makefile

---
+++ book.mk
@@ -0,0 +1,23 @@
+book := kant-perpetual-peace
+
+srcdir := src
+
+opf := content.opf
+
+content := toc.xhtml \
+           text/cover.xhtml \
+           text/titlepage.xhtml \
+           text/introduction.xhtml \
+           text/section-1.xhtml \
+           text/section-2.xhtml \
+           text/supplement-1.xhtml \
+           text/supplement-2.xhtml \
+           text/appendix-1.xhtml \
+           text/appendix-2.xhtml \
+           text/colophon.xhtml \
+           style/style.css \
+           images/cover.svg \
+           images/cover.png \
+           images/cc-zero.svg \
+           images/publicdomain.svg \
+           toc.ncx
